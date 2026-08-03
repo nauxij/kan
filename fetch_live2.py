b07841750b300b1ea9edbb891cef761010939f46
華海 <377137055@qq.com>
2026-08-03 17:04:00 +0800

    Update fetch_live2.py

--- fetch_live2.py
+++ fetch_live2.py
@@ -311,7 +311,7 @@ def sort_channels(channels):
     return sorted(channels, key=sort_key)
 
 def fetch_file():
-    url = "https://gitee.com/x21y/yx/raw/main/qdyd.nzk"
+    url = "https://gitee.com/x21y/yx/raw/main/qdyd.txt"
     out_file = "qdyd.nzk"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
